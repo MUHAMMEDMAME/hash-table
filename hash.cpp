05db50c4b38459b1a38b46abd697835f2907ac13
Muhammed Nour <118080466+MUHAMMEDMAME@users.noreply.github.com>
2024-05-13 18:42:34 +0300

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,106 +1,87 @@
-#include <iostream>
-
+#include<iostream>
+#include<vector>
+#define SIZE 10
 using namespace std;
 
-// Linked List node
-
-class Node {
-
+class Node{
 public:
-	int data;
-	Node* next;
-};
-// Hash Table
-class HashTable {
-
-public:
-	Node** HT;
-	HashTable();
-	int hash(int key);
-	void Insert(int key);
-	int Search(int key);
-	~HashTable();
+    int data;
+    Node *next;
 };
 
-HashTable::HashTable() {
-	HT = new Node * [10];
-	for (int i = 0; i < 10; i++) {
-		HT[i] = nullptr;
-	}
+class Hash{
+private:
+    Node **hashTable;
+public:
+    Hash();
+    void insertKey(int key);
+    Node * searchKey(int key);
+    int hash_function(int key);
+};
+
+int Hash::hash_function(int key){
+    return key%10;
+}
+Hash::Hash(){
+    hashTable=new Node*[10];
+    
+    for(int i=0;i<10;i++)
+        hashTable[0]=nullptr;
+    
+    
 }
 
-int HashTable::hash(int key) {
-	return key % 10;
+void Hash::insertKey(int key){
+    int hIndex=hash_function( key);
+    Node *t=new Node;
+    t->data=key;
+    t->next=nullptr;
+    
+    if(hashTable[hIndex]==NULL)
+        hashTable[hIndex]=t;
+    
+    else{
+        Node *p=hashTable[hIndex];
+        while(p->next!=NULL)
+        p=p->next;
+        
+        p->next=t;
+        
+        
+    }
+    
 }
 
-void HashTable::Insert(int key) {
-	int hIdx = hash(key);
-	Node* t = new Node;
-	t->data = key;
-	t->next = nullptr;
-	// Case: No nodes in the linked list
-	if (HT[hIdx] == nullptr) {
-		HT[hIdx] = t;
-	}
-	else {
-		Node* p = HT[hIdx];
-		Node* q = HT[hIdx];
-		// Traverse to find insert position
-		while (p && p->data < key) {
-			q = p;
-			p = p->next;
-		}
-		// Case: insert position is first
-		if (q == HT[hIdx]) {
-			t->next = HT[hIdx];
-			HT[hIdx] = t;
-		}
-		else {
-			t->next = q->next;
-			q->next = t;
-		}
-	}
+Node * Hash::searchKey(int key){
+    int hIndex=hash_function( key);
+    
+    Node *p=hashTable[hIndex];
+    
+    while(p){
+        if(p->data==key)
+            return p;
+        
+        p=p->next;
+    }
+    
+    
+    return  NULL;
+    
+    
 }
-
-int HashTable::Search(int key) {
-	int hIdx = hash(key);
-	Node* p = HT[hIdx];
-	while (p) {
-		if (p->data == key) {
-			return p->data;
-		}
-		p = p->next;
-	}
-	return -1;
-}
-
-HashTable::~HashTable() {
-	for (int i = 0; i < 10; i++) {
-		Node* p = HT[i];
-		while (HT[i]) {
-			HT[i] = HT[i]->next;
-			delete p;
-			p = HT[i];
-		}
-	}
-	delete[] HT;
-}
-
 int main() {
-	int A[] = { 16, 12, 25, 39, 6, 122, 5, 68, 75 };
-	int n = sizeof(A) / sizeof(A[0]);
-	HashTable H;
-	for (int i = 0; i < n; i++) {
-		H.Insert(A[i]);
-	}
-	cout << "Successful Search" << endl;
-	int key = 6;
-	int value = H.Search(key);
-	cout << "Key: " << key << ", Value: " << value << endl;
-	cout << "Unsuccessful Search" << endl;
-	key = 95;
-	value = H.Search(key);
-	cout << "Key: " << key << ", Value: " << value << endl;
+    vector<int> arr = {16, 12, 25, 39, 6, 122, 5, 68, 75};
+    Hash hash;
+    
+    for(int i=0;i<arr.size();i++)
+        hash.insertKey(arr[i]);
+    
+    Node *search=hash.searchKey(12);
+    
 
-	return 0;
+    if(search)
+        cout<<"Key is found:"<<search->data<<endl;
+    
+    else
+        cout<<"Key isn't found";
 }
